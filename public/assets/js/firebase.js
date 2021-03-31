0edf9e681dcd923400ad9e6cb41d52da138dfcb1
Fernando74lr <flopezramirez@hotmail.com>
2021-03-30 23:36:32 -0600

    Update firebase.js

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -4,6 +4,9 @@ firebase.initializeApp(firebaseConfig);
 // Firebase DB
 const db = firebase.firestore();
 
+const usersRef = db.collection('users');
+let emails = [];
+
 // Create user
 function createUser(user) {
     const userInfo = {
@@ -45,165 +48,84 @@ function createUser(user) {
         created: Date.now()
     };
 
-    const userQuestionsPart1 = {
-        qn1: user.pt1_1,
-        qn2: user.pt1_2,
-        qn3: user.pt1_3,
-        qn4: user.pt1_4,
-        qn5: user.pt1_5,
-        qn6: user.pt1_6,
-        qn7: user.pt1_7,
-        qn8: user.pt1_8,
-        qn9: user.pt1_9,
-        qn10: user.pt1_10,
-        qn11: user.pt1_11,
-        qn11: user.pt1_12,
-        qn13: user.pt1_13,
-        qn14: user.pt1_14,
-        qn15: user.pt1_15,
-        qn16: user.pt1_16,
-        qn17: user.pt1_17,
-        qn18: user.pt1_18,
-        qn19: user.pt1_19,
-        qn20: user.pt1_20,
-        qn21: user.pt1_21
-    };
-
-    const userQuestionsPart2 = {
-        qnIntense1: user.pt2_Intense_1 || 'No aplica',
-        qnIntense2: user.pt2_Intense_2 || 'No aplica',
-        qnModerated1: user.pt2_Moderated_1 || 'No aplica',
-        qnModerated2: user.pt2_Moderated_2 || 'No aplica',
-        qnHike1: user.pt2_Hike_1 || 'No aplica',
-        qnHike2: user.pt2_Hike_2 || 'No aplica',
-        qnSeated1: user.pt2_Seated_1 || 'No aplica'
-    };
-
-    const userQuestionsPart3 = {
-        qn1: user.pt3_1,
-        qn2: user.pt3_2,
-        qn3: user.pt3_3,
-        qn4: user.pt3_4,
-        qn5: user.pt3_5,
-        qn6: user.pt3_6,
-        qn7: user.pt3_7,
-        qn8: user.pt3_8,
-        qn9: user.pt3_9,
-        qn10: user.pt3_10
-    };
-
-    const userQuestionsPart4 = {
-        qn1: user.pt4_1,
-        qn2: user.pt4_2,
-        qn3: user.pt4_3,
-        qn4: user.pt4_4,
-        qn5: user.pt4_5,
-        qn6: user.pt4_6
-    };
-
-    const userQuestionsPart5 = {
-        qn1: user.pt5_1,
-        qn2: user.pt5_2,
-        qn3: user.pt5_3,
-        qn4: user.pt5_4,
-        qn5: user.pt5_5,
-        qn6: user.pt5_6,
-        qn7: user.pt5_7,
-        qn8: user.pt5_8
+    const questions = {
+        pt1_1: user.pt1_1,
+        pt1_2: user.pt1_2,
+        pt1_3: user.pt1_3,
+        pt1_4: user.pt1_4,
+        pt1_5: user.pt1_5,
+        pt1_6: user.pt1_6,
+        pt1_7: user.pt1_7,
+        pt1_8: user.pt1_8,
+        pt1_9: user.pt1_9,
+        pt1_10: user.pt1_10,
+        pt1_11: user.pt1_11,
+        pt1_12: user.pt1_12,
+        pt1_13: user.pt1_13,
+        pt1_14: user.pt1_14,
+        pt1_15: user.pt1_15,
+        pt1_16: user.pt1_16,
+        pt1_17: user.pt1_17,
+        pt1_18: user.pt1_18,
+        pt1_19: user.pt1_19,
+        pt1_20: user.pt1_20,
+        pt1_21: user.pt1_21,
+        pt2_Intense_1: user.pt2_Intense_1 || 'No aplica',
+        pt2_Intense_2: user.pt2_Intense_2 || 'No aplica',
+        pt2_Moderated_1: user.pt2_Moderated_1 || 'No aplica',
+        pt2_Moderated_2: user.pt2_Moderated_2 || 'No aplica',
+        pt2_Hike_1: user.pt2_Hike_1 || 'No aplica',
+        pt2_Hike_2: user.pt2_Hike_2 || 'No aplica',
+        pt2_Seated_1: user.pt2_Seated_1 || 'No aplica',
+        pt3_1: user.pt3_1,
+        pt3_2: user.pt3_2,
+        pt3_3: user.pt3_3,
+        pt3_4: user.pt3_4,
+        pt3_5: user.pt3_5,
+        pt3_6: user.pt3_6,
+        pt3_7: user.pt3_7,
+        pt3_8: user.pt3_8,
+        pt3_9: user.pt3_9,
+        pt3_10: user.pt3_10,
+        pt4_1: user.pt4_1,
+        pt4_2: user.pt4_2,
+        pt4_3: user.pt4_3,
+        pt4_4: user.pt4_4,
+        pt4_5: user.pt4_5,
+        pt4_6: user.pt4_6,
+        pt5_1: user.pt5_1,
+        pt5_2: user.pt5_2,
+        pt5_3: user.pt5_3,
+        pt5_4: user.pt5_4,
+        pt5_5: user.pt5_5,
+        pt5_6: user.pt5_6,
+        pt5_7: user.pt5_7,
+        pt5_8: user.pt5_8
     };
 
     // Add a new user in collection 'users'
-    // const res = await 
-    db.collection('users').doc(user.name).set(userInfo);
-    db.collection(`users/${user.name}/questions`).doc('part1').set(userQuestionsPart1);
-    db.collection(`users/${user.name}/questions`).doc('part2').set(userQuestionsPart2);
-    db.collection(`users/${user.name}/questions`).doc('part3').set(userQuestionsPart3);
-    db.collection(`users/${user.name}/questions`).doc('part4').set(userQuestionsPart4);
-    db.collection(`users/${user.name}/questions`).doc('part5').set(userQuestionsPart5);
-    console.log("USER CREATED");
+    try {
+        usersRef.doc(user.name).set({
+            userInfo: userInfo,
+            questions: questions
+        });
+        console.log("USER CREATED");
+    } catch (error) {
+        console.log("Something went wrong trying to create the user :/");
+        console.log("ERROR: " + error);
+    }
 }
 
-// const userTest = {
-//     firstMessage: true,
-//     consent: true,
-//     image: {
-//         name: "pp9.PNG",
-//         type: "image/png",
-//         content: "data:image/png;base64"
-//     },
-//     facultyYear: "2000",
-//     name: "VHHS",
-//     age: 20,
-//     gender: "Masculino",
-//     civilStatus: "Soltero (a)",
-//     kids: false,
-//     state: "Puebla",
-//     municipality: "Puebla",
-//     typeOfResidence: "Urbano",
-//     email: "flopezramirez@hotmail.com",
-//     emailConfirmation: "flopezramirez@hotmail.com",
-//     occupation: "Alumno",
-//     semester: "7mo",
-//     worked: true,
-//     seriousWorkProblems: "Nunca o casi nunca",
-//     accessToInternetAndDevicesStudent: "Fácil de obtener",
-//     academicPerformance: "Tuve menor aprovechamiento",
-//     difficultiesToStudy: "Nunca o casi nunca",
-//     pt1_1: "No me ha ocurrido",
-//     pt1_2: "No me ha ocurrido",
-//     pt1_3: "No me ha ocurrido",
-//     pt1_4: "No me ha ocurrido",
-//     pt1_5: "No me ha ocurrido",
-//     pt1_6: "No me ha ocurrido",
-//     pt1_7: "No me ha ocurrido",
-//     pt1_8: "No me ha ocurrido",
-//     pt1_9: "No me ha ocurrido",
-//     pt1_10: "No me ha ocurrido",
-//     pt1_11: "No me ha ocurrido",
-//     pt1_12: "No me ha ocurrido",
-//     pt1_13: "No me ha ocurrido",
-//     pt1_14: "No me ha ocurrido",
-//     pt1_15: "No me ha ocurrido",
-//     pt1_16: "No me ha ocurrido",
-//     pt1_17: "No me ha ocurrido",
-//     pt1_18: "No me ha ocurrido",
-//     pt1_19: "No me ha ocurrido",
-//     pt1_20: "No me ha ocurrido",
-//     pt1_21: "No me ha ocurrido",
-//     typeOfActivity: "Sentado (nula actividad física)",
-//     pt2_Seated_1: 10,
-//     pt3_1: true,
-//     pt3_2: false,
-//     pt3_3: false,
-//     pt3_4: true,
-//     pt3_5: true,
-//     pt3_6: true,
-//     pt3_7: true,
-//     pt3_8: true,
-//     pt3_9: true,
-//     pt3_10: true,
-//     useOfMask: "Casi siempre",
-//     typeOfMask: "KN95",
-//     pt4_1: true,
-//     pt4_2: false,
-//     pt4_3: true,
-//     pt4_4: true,
-//     pt4_5: false,
-//     pt4_6: true,
-//     pt5_1: "Nunca o casi nunca",
-//     pt5_2: "Nunca o casi nunca",
-//     pt5_3: "Nunca o casi nunca",
-//     pt5_4: "Nunca o casi nunca",
-//     pt5_5: "Nunca o casi nunca",
-//     pt5_6: "Bastantes veces",
-//     pt5_7: "Algunas veces",
-//     pt5_8: "Algunas veces"
-// };
-
-
-// test();
-
-// function test() {
-//     createUser(userTest);
-//     console.log("User loaded");
-// }
+usersRef.get()
+    .then(function(snapshot) {
+        if (snapshot) {
+            snapshot.forEach(doc => {
+                emails.push(doc.data().email);
+            });
+        }
+        else {
+            console.log("No data available (when getting emails)");
+        }
+    }).catch(function(error) {
+      console.error(error);
+    });
